593012da4b03022916a777a52423d96889cbd9fc
seeker <populpock2@gmail.com>
2020-12-15 06:31:36 +0300

    Release v1.0 (Added Commentary)

--- main.c
+++ main.c
@@ -2,44 +2,35 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#include <stdlib.h> // JUST FOR SYSTEM() FUNCTION
 
-// 2 8 5 6 4
-
-void sorter(int *arr, int j) 
+void sorter(int *arr, int j) // we don't need to copy the whole array, better use pointers
 {
     int c, i = 0;
 
     while (i != j)
     {
-        for (int R = 0; R != 5; R++)
+        if (arr[i] % 2 != 0) // if (odd)
         {
-            printf("%d ", arr[R]);
-        }
-        printf("\n");
-
-        if (arr[i] % 2 != 0)
-        {
-            if (arr[j] % 2 == 0)
+            if (arr[j] % 2 == 0) // if (even)
             {
-                c = arr[i];
+                c = arr[i];         // swapping
                 arr[i] = arr[j];
                 arr[j] = c;
 
                 if (i != j - 1)
                     i++, j--;
                 else
-                    break;
+                    break; // if current - even, next - odd, at this point task is completed
             }
             else
                 j--;
         }
-        else 
+        else
         {
-            if (arr[j] % 2 == 0)
+            if (arr[j] % 2 == 0) // if arr[j] is even - let it be
                 i++;
             else
-                i++, j--;
+                i++, j--; // if arr[j] is odd - skip it
         }
     }
 }
@@ -49,7 +40,7 @@ int main() {
     int size = 0; // size of array
 
     printf("\n Enter array length: ");
-    if (scanf("%d", &size) != 1 || size > 100 || size < 0 || size % 1 != 0)
+    if (scanf("%d", &size) != 1 || size > 100 || size < 0 || size % 1 != 0) // validation
     {
         printf("\n Input error, try again, using integer number.\n ");
         return 0;
@@ -61,7 +52,7 @@ int main() {
     for (; j != size + 1; j++)
     {
         printf(" Enter array element number %d: ", j + 1);
-        if (scanf("%d", &arr[j]) != 1 || arr[j] < 0 || arr[j] % 1 != 0)
+        if (scanf("%d", &arr[j]) != 1 || arr[j] < 0 || arr[j] % 1 != 0) // validation
         {
             printf("\n Input error, try again, using integer number.\n ");
             return 0;
